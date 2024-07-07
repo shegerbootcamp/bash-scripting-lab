62164e3b5b7597d0883a0db287deb0a473bc0460
Jimi M <jimi@Jimis-MBP.cable.rcn.com>
2024-07-06 20:34:50 -0400

    vscode install

--- vscode-install.sh
+++ vscode-install.sh
@@ -6,8 +6,8 @@ sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
 
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
+sudo apt install apt-transport-https -y
+sudo apt update -y
+sudo apt install code -y
 
 
